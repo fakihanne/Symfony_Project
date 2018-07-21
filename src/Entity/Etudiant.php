43ea439938a2551af5cf702bbd24cf263f746ac4
fatou hanne <fakihanne92@gmail.com>
2018-07-21 20:45:52 +0000

    Création Entité Etudiant et du crud associé

---
+++ src/Entity/Etudiant.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\EtudiantRepository")
+ */
+class Etudiant
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $Prenom;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $Nom;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $Adresse;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getPrenom(): ?string
+    {
+        return $this->Prenom;
+    }
+
+    public function setPrenom(string $Prenom): self
+    {
+        $this->Prenom = $Prenom;
+
+        return $this;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->Nom;
+    }
+
+    public function setNom(string $Nom): self
+    {
+        $this->Nom = $Nom;
+
+        return $this;
+    }
+
+    public function getAdresse(): ?string
+    {
+        return $this->Adresse;
+    }
+
+    public function setAdresse(string $Adresse): self
+    {
+        $this->Adresse = $Adresse;
+
+        return $this;
+    }
+}
